9f0f01d9a1e2720d7f44602b721cb5433e36d670
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-01-22 01:10:51 +0000

    hasta la primer card

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import Card from "./Card.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,18 +10,25 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<div className="container">
+				<div className="row">
+					<div
+						className="col"
+						style={{ marginTop: "25px", marginBottom: "25px" }}>
+						<Jumbotron />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">card2</div>
+					<div className="col-3">card3</div>
+					<div className="col-3">card4</div>
+				</div>
+				footer
+			</div>
 		</div>
 	);
 };
